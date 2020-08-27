01efcb21f3a6d040ab2d91c730ebc9717e59b7b2
ostbobmuon <ostbobmuon@gmail.com>
2020-08-27 12:49:04 +0300

    1. String style changed 2. print to puts 3. max_side logic refactoring

--- lesson_01/right_triangle_check.rb
+++ lesson_01/right_triangle_check.rb
@@ -1,26 +1,14 @@
-print "Give me first side of triangle:\n"
-first_side = Integer(gets.chomp)
-print "Give me second side of triangle:\n"
-second_side = Integer(gets.chomp)
-print "Give me third side of triangle:\n"
-third_side = Integer(gets.chomp)
+puts 'Give me first side of triangle:'
+first_side = gets.chomp.to_f
+puts 'Give me second side of triangle:'
+second_side = gets.chomp.to_f
+puts 'Give me third side of triangle:'
+third_side = gets.chomp.to_f
 
-if first_side >= second_side && first_side >= third_side
-  max_side = first_side
-  first_catet = second_side
-  second_catet = third_side
-elsif second_side >= first_side && second_side >= third_side
-  max_side = second_side
-  first_catet = first_side
-  second_catet = third_side
-else
-  max_side = third_side
-  first_catet = first_side
-  second_catet = second_side
-end
+first_catet, second_catet, max_side = [first_side, second_side, third_side].sort
 
 if max_side**2 == first_catet **2 + second_catet**2
-  print "Triangle is right\n"
+  puts 'Triangle is right'
 else
-  print "Triangle is not right\n"
+  puts 'Triangle is not right'
 end
