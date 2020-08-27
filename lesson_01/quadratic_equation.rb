69b2c7f343c0d338434f08f4092b2093e81e9061
ostbobmuon <ostbobmuon@gmail.com>
2020-08-27 12:45:42 +0300

    1. String style changed 2. Added sqrt_of_D variable

--- lesson_01/quadratic_equation.rb
+++ lesson_01/quadratic_equation.rb
@@ -1,23 +1,24 @@
-print "Give me a, b, c coefficients for quadratic equation please.\n"
-print "Give me a:\n"
-a = Integer(gets.chomp)
-print "Give me b:\n"
-b = Integer(gets.chomp)
-print "Give me c:\n"
-c = Integer(gets.chomp)
+puts 'Give me a, b, c coefficients for quadratic equation please.'
+puts 'Give me a:'
+a = gets.chomp.to_f
+puts 'Give me b:'
+b = gets.chomp.to_f
+puts 'Give me c:'
+c = gets.chomp.to_f
 
 D = b**2 - 4*a*c
 
 if D < 0
-  print "Корней нет\n"
+  puts 'Корней нет'
 elsif D == 0
-  print "Дискриминант: #{D}\n"
-  x = -b/2*a
-  print "Корень: #{x}\n"
+  puts "Дискриминант: #{D}"
+  x = -b/(2*a)
+  puts "Корень: #{x}"
 elsif D > 0
-  x1 = (-b + Math.sqrt(D))/(2*a)
-  x2 = (-b - Math.sqrt(D))/(2*a)
-  print "Дискриминант: #{D}\n"
-  print "Корень x1: #{x1}\n"
-  print "Корень x2: #{x2}\n"
+  sqrt_of_D = Math.sqrt(D)
+  x1 = (-b + sqrt_of_D)/(2*a)
+  x2 = (-b - sqrt_of_D)/(2*a)
+  puts "Дискриминант: #{D}"
+  puts "Корень x1: #{x1}"
+  puts "Корень x2: #{x2}"
 end
