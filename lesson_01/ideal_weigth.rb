ff09f64cff31439bd0d29707ce33a1e9fe2c9581
ostbobmuon <ostbobmuon@gmail.com>
2020-08-27 12:44:51 +0300

    1. Print change to puts 2.String stryle changed

--- lesson_01/ideal_weigth.rb
+++ lesson_01/ideal_weigth.rb
@@ -1,12 +1,12 @@
-print "What is your name ?\n"
+puts 'What is your name ?'
 name = gets.chomp
-print "What is your height?\n"
-height = Integer(gets.chomp)
+puts 'What is your height?'
+height = gets.chomp.to_f
 
 ideal_weight = (height - 110) * 1.15
 
 if ideal_weight < 0
-  print "Ваш вес уже оптимальный"
+  puts 'Ваш вес уже оптимальный'
 else
-  print "#{name}, your ideal weight is #{ideal_weight}\n"
+  puts "#{name}, your ideal weight is #{ideal_weight}."
 end
