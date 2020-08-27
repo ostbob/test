aa48e509cf8bc4bbee6d0f8b06ada49bfcf618d8
ostbobmuon <ostbobmuon@gmail.com>
2020-08-27 08:36:46 +0300

    triangle_square.rb added

---
+++ lesson_01/triangle_square.rb
@@ -0,0 +1,8 @@
+print "Give me base of triangle:\n"
+base = Integer(gets.chomp)
+print "Give me height of triangle:\n"
+height = Integer(gets.chomp)
+
+square = base * height / 2.0
+
+print "Square of triangle : #{square} \n"
