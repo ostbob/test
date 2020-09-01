2963e9b54e82b33db0c41a2468793c4fc312dfdb
ostbobmuon <ostbobmuon@gmail.com>
2020-09-01 20:30:08 +0300

    built-in methods were deleted

--- part_03/task05.rb
+++ part_03/task05.rb
@@ -1,5 +1,3 @@
-require 'date'
-
 puts 'Input your day:'
 day = gets.chomp.to_i
 puts 'Input your month:'
@@ -7,7 +5,27 @@ month = gets.chomp.to_i
 puts 'Inputs your year:'
 year = gets.chomp.to_i
 
-your_date = Date.new(year, month, day)
-year_first_date = Date.new(year, 1, 1)
 
-puts (your_date - year_first_date + 1).to_i
+month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if year % 400 == 0
+  year_is_leap = true
+elsif year % 100 == 0
+  year_is_leap = false
+elsif year % 4 == 0
+  year_is_leap = true
+else
+  year_is_leap = false
+end
+
+if year_is_leap
+  month_days[1] = 29
+end
+
+your_day_number = day
+
+month_days[0...month-1].each do |days|
+  your_day_number += days
+end
+
+puts your_day_number
